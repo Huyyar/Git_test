1175749d74bcd428a06730d66326d70ef0be1f27
hya <hya@gmail.com>
2025-10-25 20:34:26 +0700

    creat  HelloWorld

--- src/FileAa.java
+++ src/FileAa.java
@@ -2,5 +2,6 @@ public class FileAa {
     public static void main(String[] args) {
         System.out.println("B đã sửa ");
         System.out.println("main commit ");
+        System.out.println("Hello word");
     }
 }
